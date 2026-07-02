7901b9027a6a1f254db701bf631877baed26c099
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-07-03 00:56:34 +0600

    using context, signin user login form and on auth state change

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../context/AuthContext';
 
 const Login = () => {
-    return (
+  const { signInUser } = use(AuthContext);
+
+  const handleLogin = e => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+    signInUser(email, password)
+      .then(result => console.log(result))
+      .catch(error => console.log(error))
+  }
+  return (
     <div className="card bg-base-100 mt-10 mx-auto w-full max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
         <h1 className="text-3xl font-bold">Login now!</h1>
-        <form className="fieldset">
+        <form onSubmit={handleLogin} className="fieldset">
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input name='email' type="email" className="input" placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input name='password' type="password" className="input" placeholder="Password" />
           <div><a className="link link-hover">Forgot password?</a></div>
           <button className="btn btn-neutral mt-4">Login</button>
         </form>
-        <p>Already have an account ? please<Link className='text-blue-700 underline' to={'/login'}>Login</Link></p>
+        <p>New to this site ? please<Link className='text-blue-700 underline' to={'/register'}>Register</Link></p>
       </div>
     </div>
-    );
+  );
 };
 
 export default Login;
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase/firebase.init';
 
 const AuthProvider = ({ children }) => {
@@ -8,8 +8,21 @@ const AuthProvider = ({ children }) => {
     const createUser = (email, password) =>{
         return createUserWithEmailAndPassword(auth, email, password )
     }
+    const signInUser = (email, password) =>{
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    onAuthStateChanged(auth, (currentUser)=>{
+        if(currentUser){
+            console.log("has current user", currentUser);
+        }
+        else{
+            console.log('current user',currentUser);
+        }
+    })
+
     const userInfo = {
-        createUser
+        createUser,
+        signInUser
     }
 
     return (
